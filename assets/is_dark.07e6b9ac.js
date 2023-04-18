5eb60d1aa411a97141008f24e0e129f0710f61b0
yuhan6665 <yuhan6665@users.noreply.github.com>
2023-04-18 03:48:40 +0000

    deploy: ab047ff01f07b1d832769201d6738c6a1110a82e

---
+++ assets/is_dark.07e6b9ac.js
@@ -0,0 +1,1 @@
+import{aA as a,aB as n}from"./mermaid.core.b02af540.js";const r=r=>(r=>{const{r:e,g:o,b:t}=n.parse(r),s=.2126*a.channel.toLinear(e)+.7152*a.channel.toLinear(o)+.0722*a.channel.toLinear(t);return a.lang.round(s)})(r)>=.5,e=a=>!r(a);export{e as i};
